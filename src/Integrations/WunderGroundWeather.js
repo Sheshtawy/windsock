c1a4b2a86b67a22efac24ca3a214640177d4eda2
Sheshtawy <hisham.elsheshtawy@gmail.com>
2018-01-20 13:37:23 -0500

    Add WunderGround client implementation
    Add private method to extend wunderground links into respective condition result

---
+++ src/Integrations/WunderGroundWeather.js
@@ -0,0 +1,58 @@
+const _ = require('lodash');
+const request = require('request-promise-native');
+const BaseClient = require('./BaseClient');
+
+class WunderGroundWeather extends BaseClient {
+
+    constructor(config) {
+        super(config);
+
+        if(!_.isString(config.key)) {
+            throw new Error('Invalid or missing WonderGroundWeather api key');
+        }
+    }
+
+    getConditionByCity(city, state = '', country = '') {
+        if(!_.isString(city)) {
+            throw new Error('Invalid city name. Make sure it\'s a non-empty string');
+        }
+
+        const params = [];
+        
+        if(!_.isEmpty(country)) {
+            params.push(country);
+        }
+        
+        if(!_.isEmpty(state)) {
+            params.push(state);
+        }
+
+        params.push(city);
+        const query = params.join('/');
+        return this._call('GET', 'conditions', query)
+            .then(response => {
+                if('results' in response && _.isArray(response.results)) {
+                    return _.map(response.results, item => this._extendResponse(item.l));
+                }
+            });
+    }
+
+    getConditionById() {}
+
+    _extendResponse(link) {
+        return this._call('GET', 'conditions', link);
+    }
+
+    _call(method, endpoint, query) {
+        const requestObject = {
+            method,
+            uri: `http://api.wunderground.com/api/${this.config.key}/${endpoint}/q/${query}.json`,
+            json: true,
+        };
+
+        return request(requestObject);
+    }
+
+}
+
+module.exports = WunderGroundWeather;
